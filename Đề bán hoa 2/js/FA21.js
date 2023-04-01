7b1609b76d221914b5d12a0a2e2330a2b9acd2fb
Do-Thomas <94061934+Do-Thomas@users.noreply.github.com>
2023-04-01 19:10:05 +0700

    Add files via upload

---
+++ Đề bán hoa 2/js/FA21.js
@@ -0,0 +1,19 @@
+function customerBuy() {
+    var phone = prompt("Please enter your phone number: ");
+    if(phone.length != 0 && parseInt(phone)==phone) {
+      alert('We will contact you after 30 min')
+    }
+ }
+
+ //var check = confirm("mess sent");
+ //if(chek==1)  //(true) {
+  //   location.reload();
+ //}
+
+
+
+ // click vao anh no hien ra anh do  (zoomout)
+//function zoomout(img) {
+  // var linkImage = img.src;
+//   document.getElementByID('frame').style.backgroundImage='url(' + linkImage + ')';
+//}
