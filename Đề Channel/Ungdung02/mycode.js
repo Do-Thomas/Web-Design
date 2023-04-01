cc3da5dcc327203b13e953ac68ae28f3d7e52bb4
Do-Thomas <94061934+Do-Thomas@users.noreply.github.com>
2023-04-01 19:11:01 +0700

    Add files via upload

---
+++ Đề Channel/Ungdung02/mycode.js
@@ -0,0 +1,11 @@
+// function Color(color){
+//     let color = document.getElementById('color').style.input.color = "yellow"
+//     // document.body.style.input = color;
+// }
+
+function Color_Background() {
+    // Color('yellow');
+    // document.getElementById("color").style.backgroundOrigin.input = "yellow";
+    // document.body.style.background = 'yellow';
+
+}
